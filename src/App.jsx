4a747f545023d6cd094f4c62f8081e4335be9173
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2021-09-08 12:59:51 +0530

    initialized chakra ui and tt

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Table from './Components/Table.jsx';
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
+      <Table />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import { ChakraProvider } from "@chakra-ui/react"
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  , document.getElementById('root')
 );
+
+
+export default App;
